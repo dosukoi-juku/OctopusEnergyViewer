c12e7b7eeef26ae804effd8d9f14073994026f41
Miyayu <yuchan2215@gmail.com>
2023-09-09 19:17:01 +0900

    add: ktlintを導入

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
-    //trick: for the same plugin versions in all sub-modules
+    // trick: for the same plugin versions in all sub-modules
     alias(libs.plugins.androidApplication).apply(false)
     alias(libs.plugins.androidLibrary).apply(false)
     kotlin("android").version(libs.versions.kotlin).apply(false)
     kotlin("multiplatform").version(libs.versions.kotlin).apply(false)
+
+    alias(libs.plugins.ktlint)
 }
 
 tasks.register("clean", Delete::class) {
