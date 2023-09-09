fb010b6bb4e2f1fc1cd476a9a6591631d93ecfb3
Miyayu <yuchan2215@gmail.com>
2023-09-09 19:11:30 +0900

    migrate: VersionCatalogを導入

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -40,10 +40,6 @@ android {
 
 dependencies {
     implementation(project(":shared"))
-    implementation("androidx.compose.ui:ui:1.4.3")
-    implementation("androidx.compose.ui:ui-tooling:1.4.3")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.4.3")
-    implementation("androidx.compose.foundation:foundation:1.4.3")
-    implementation("androidx.compose.material:material:1.4.3")
-    implementation("androidx.activity:activity-compose:1.7.1")
+    implementation(platform(libs.compose.bom))
+    implementation(libs.bundles.composeBundles)
 }
