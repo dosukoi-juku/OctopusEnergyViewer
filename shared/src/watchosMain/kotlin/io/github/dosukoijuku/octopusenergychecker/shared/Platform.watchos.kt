6c90ea048a8a7e540a8ec14e300619d17cd9abb8
Miyayu <yuchan2215@gmail.com>
2023-11-05 18:04:54 +0900

    Revert "add: WatchOSの環境を追加"
    This reverts commit 563458a1

--- shared/src/watchosMain/kotlin/io/github/dosukoijuku/octopusenergychecker/shared/Platform.watchos.kt
+++
@@ -1,9 +0,0 @@
-package io.github.dosukoijuku.octopusenergychecker.shared
-
-import platform.WatchKit.WKInterfaceDevice
-
-class WatchOSPlatform: Platform {
-    override val name: String = WKInterfaceDevice.currentDevice().systemName() + " " + WKInterfaceDevice.currentDevice().systemVersion()
-}
-
-actual fun getPlatform(): Platform = WatchOSPlatform()
