8e36b822f67544cd6275c60a6fcf7ef1eb5c76da
Miyayu <yuchan2215@gmail.com>
2023-11-05 20:29:52 +0900

    WIP: ログイン状態を示すカードを作成

--- androidApp/src/main/java/io/github/dosukoijuku/octopusenergychecker/android/view/compose/setting/SettingScreen.kt
+++ androidApp/src/main/java/io/github/dosukoijuku/octopusenergychecker/android/view/compose/setting/SettingScreen.kt
@@ -1,24 +1,43 @@
 package io.github.dosukoijuku.octopusenergychecker.android.view.compose.setting
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import io.github.dosukoijuku.octopusenergychecker.android.R
+import io.github.dosukoijuku.octopusenergychecker.android.view.compose.common.LoginStateCard
+import io.github.dosukoijuku.octopusenergychecker.android.view.compose.common.LoginStateCardUiState
 import io.github.dosukoijuku.octopusenergychecker.android.view.theme.OctopusTheme
 
 @Composable
 fun SettingScreen() {
+
+    //TODO ViewModelに吸収させる
+    val loginStateCardUiState by remember { mutableStateOf(LoginStateCardUiState.NotLoggedIn) }
+
     Scaffold(
         topBar = { SettingScreenTopBar() }
     ) {
-        Box(modifier = Modifier.padding(it))
+        Column(
+            modifier =
+            Modifier
+                .padding(it)
+                .fillMaxWidth(),
+        ) {
+            LoginStateCard(
+                uiState = loginStateCardUiState
+            )
+        }
     }
 }
 
